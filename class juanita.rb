21aabefc59ea127d1ece6d476b38c52f1a6a8fc7
Edgar Paz Moreno <epazmore@gmail.com>
2016-11-09 19:24:48 -0600

    second commit

---
+++ class juanita.rb
@@ -0,0 +1,19 @@
+class juanita
+
+	attr :edad, :nombre, :dirección
+	class << self
+	class << abcd
+	class << dos_edades 
+
+		def initialize(edad = 20, nombre = "Juanita", direccion = "3rd nose street", tareas = 10)
+			self.edad = edad
+			self.nombre = nombre
+			self.direccion = direccion
+		end
+
+		def lavar_la_tina
+			self.nombre
+			@tareas -= 1
+		end
+	end		
+end
